08c0c06576097d446b66c25d54e1f41b1a1aee04
Daniel Edminster <Daniel.Edminster@outlook.com>
2020-03-09 15:34:16 -0400

    Added substantial game logic, including war card cache

--- js/main.js
+++ js/main.js
@@ -4,14 +4,13 @@ let deck = [] ;
 let playerDeck = [];
 let aiDeck = [];
 
+let playerWarCardCache = [];
+let AIWarCardCache = [];
+let warActive = false;
+
 function generateDeck()
 {
-    // let card = [
-    //     {
-    //         suit: "",
-    //         num: 0
-    //     }
-    // ];
+
 
     let counter = 0;
 
@@ -81,27 +80,22 @@ function generateDeck()
     }
         shuffle(deck);
         assignDecks();
+        console.log("Type turn(); to play.")
+        // turn();
 }
 
 function assignDecks()
 {
-    // playerDeck.push(
-    //     deck.slice(0, Math.ceil(deck.length / 2))
-    // );
 
+    //split the shuffled deck in two
     for(let i=0;i<deck.length;i++)
     {
         i % 2 == 0 ? playerDeck.push(deck[i]) : aiDeck.push(deck[i]);
   
     }
     
-
-    // aiDeck.push(
-    //     deck.slice(Math.ceil(deck.length / 2), deck.length)
-    // );
-
-    console.log("PlayerDeck: ", playerDeck);
-    console.log("aiDeck: ", aiDeck);
+    // console.log("PlayerDeck: ", playerDeck);
+    // console.log("aiDeck: ", aiDeck);
 }
 
 
@@ -112,4 +106,122 @@ function shuffle(array) {
 
 function randomNumber(min, max) {  
     return Math.floor(Math.random() * (max - min) + min); 
+}
+
+function turn()
+{
+    // console.log("aiDeck: ", aiDeck);
+    // console.log("playerDeck: ", playerDeck);
+    let aiCard = aiDeck.shift();
+    // aiDeck.unshift(aiCard0);
+    let playerCard = playerDeck.shift();
+    // playerDeck.unshift(playerCard0)
+    // let aiCard = aiDeck.pop();
+    // let playerCard = playerDeck.pop(); 
+
+    console.log("Your Card: ", playerCard.suit, playerCard.num);
+    console.log("AI Card: ", aiCard.suit , aiCard.num);
+
+    if(playerCard.num > aiCard.num)
+    {
+        console.log("You won this round.");
+        playerDeck.push(playerCard);
+        playerDeck.push(aiCard);
+        console.log("Your Deck: ", playerDeck.length, "AI Deck: ", aiDeck.length);
+    }
+    else if(playerCard.num < aiCard.num)
+    {
+        console.log("You lost this round.");
+        aiDeck.push(playerCard);
+        aiDeck.push(aiCard);
+        console.log("Your Deck: ", playerDeck.length, "AI Deck: ", aiDeck.length);
+    }
+    else {
+        console.log("War!");
+        war();
+        clearWarCardCaches();
+    }
+
+}
+
+function war()
+{
+    playerWarCardCache.push(playerCard);
+    AIWarCardCache.push(aiCard);
+
+    if(playerDeck.length < 4)
+    {
+        console.log("AI wins! You ran out of cards for war.");
+    }
+    else if(aiDeck.length < 4)
+    {
+        console.log("You win! AI ran out of cards for war.");
+    }
+    else 
+    {
+        
+        for (let i = 0;i < 4;i++)
+        {
+            playerWarCardCache.push(playerDeck.shift());
+            AIWarCardCache.push(aiDeck.shift());
+        }
+
+        if(playerWarCardCache[(playerWarCardCache.length-1)].num > AIWarCardCache[(AIWarCardCache.length-1)].num)
+        {
+            console.log("Your play: ", playerWarCardCache[(playerWarCardCache.length-1)].suit, playerWarCardCache[(playerWarCardCache.length-1)].num);
+            console.log("AI play: ", AIWarCardCache[(AIWarCardCache.length-1)].suit, AIWarCardCache[(AIWarCardCache.length-1)].num);
+            console.log("You won war!");
+            console.log("Cards won:");
+
+            for(let j=0; j < AIWarCardCache.length; j++)
+            {
+                console.log(AIWarCardCache[j].suit, AIWarCardCache[j].num);
+                playerDeck.push(AIWarCardCache[j]);
+            }
+            for(let x=0; x < playerWarCardCache.length; x++)
+            {
+                playerDeck.push(playerWarCardCache[x]);
+            }
+            
+
+        }
+        else if(playerWarCardCache[(playerWarCardCache.length-1)].num < AIWarCardCache[(AIWarCardCache.length-1)].num)
+        {
+            console.log("Your play: ", playerWarCardCache[(playerWarCardCache.length-1)].suit, playerWarCardCache[(playerWarCardCache.length-1)].num);
+            console.log("AI play: ", AIWarCardCache[(AIWarCardCache.length-1)].suit, AIWarCardCache[(AIWarCardCache.length-1)].num);
+            console.log("You lost war!");
+            console.log("Cards lost:");
+            for(let k=0; k < AIWarCardCache.length; k++)
+            {
+                console.log(playerWarCardCache[k].suit, playerWarCardCache[k].num);
+                aiDeck.push(playerWarCardCache[k]);
+            }
+            for(let y=0; y < playerWarCardCache.length; y++)
+            {
+                aiDeck.push(AIWarCardCache[y]);
+            }
+        }
+        else {
+            war();
+        }
+
+    }
+}
+
+function checkWinner()
+{
+    if(playerDeck.length === 0)
+    {
+        console.log("AI wins!");
+    }
+    else if(aiDeck.length === 0)
+    {
+        console.log("You Win!");
+    }
+}
+
+function clearWarCardCaches()
+{
+    playerWarCardCache = [];
+    AIWarCardCache = [];
 }
