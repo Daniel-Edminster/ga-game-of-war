241a0f851500a0a5fe54f3b9761e4620ecd1da40
Daniel Edminster <Daniel.Edminster@outlook.com>
2020-03-10 23:15:15 -0400

    working out game logic bugs, added more GUI stuff

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ let aiDeck = [];
 let playerWarCardCache = [];
 let AIWarCardCache = [];
 let warActive = false;
+let recursiveWar = false;
 
 function generateDeck()
 {
@@ -21,7 +22,8 @@ function generateDeck()
                 card = 
                     {
                         suit: "spade",
-                        num: i+2
+                        num: i+2,
+                        suitColor: 'black'
                     };
 
                 deck[counter] = card;
@@ -32,7 +34,8 @@ function generateDeck()
                 card = 
                     {
                         suit: "heart",
-                        num: i+2
+                        num: i+2,
+                        suitColor: 'red'
                     };
                 // card.suit = "heart";
                 // card.num = i+2;
@@ -45,7 +48,8 @@ function generateDeck()
                 card = 
                     {
                         suit: "diamond",
-                        num: i+2
+                        num: i+2,
+                        suitColor: 'red'
                     };
                 // card.suit = "diamond";
                 // card.num = i+2;
@@ -60,7 +64,8 @@ function generateDeck()
                 card = 
                     {
                         suit: "club",
-                        num: i+2
+                        num: i+2,
+                        suitColor: 'black'
                     };
                 deck[counter] = card;
 
@@ -117,11 +122,7 @@ function turn()
     // let aiCard = aiDeck.pop();
     // let playerCard = playerDeck.pop(); 
 
-    nums = document.getElementsByClassName("card");
-    document.getElementById("playerScore").innerHTML="You: "+playerDeck.length;
-    document.getElementById("aiScore").innerHTML= "AI: "+aiDeck.length;
-    nums[0].innerHTML=playerCard.num;
-    nums[1].innerHTML=aiCard.num;
+
     
 
     console.log("Your Card: ", playerCard.suit, playerCard.num);
@@ -133,6 +134,7 @@ function turn()
         playerDeck.push(playerCard);
         playerDeck.push(aiCard);
         console.log("Your Deck: ", playerDeck.length, "AI Deck: ", aiDeck.length);
+
     }
     else if(playerCard.num < aiCard.num)
     {
@@ -140,6 +142,7 @@ function turn()
         aiDeck.push(playerCard);
         aiDeck.push(aiCard);
         console.log("Your Deck: ", playerDeck.length, "AI Deck: ", aiDeck.length);
+
     }
     else {
         console.log("War!");
@@ -147,22 +150,39 @@ function turn()
         war();
         clearWarCardCaches();
     }
+    // nums = document.getElementsByClassName("card");
+    // document.getElementById("playerScore").innerHTML="You: "+playerDeck.length;
+    // document.getElementById("aiScore").innerHTML= "AI: "+aiDeck.length;
+    // nums[0].innerHTML=playerCard.num;
+    // nums[1].innerHTML=aiCard.num;
+
+    renderCardsToBrowser(playerCard, aiCard);
+    renderScoresToBrowser();
+    return;
 
 }
 
 // function war(playerCard, aiCard)
-function war()
+function war(playerCard = '', aiCard = '')
 {
     // playerWarCardCache.push(playerCard);
     // AIWarCardCache.push(aiCard);
+    if(!recursiveWar)
+    {
+            playerWarCardCache.push(playerCard);
+            AIWarCardCache.push(aiCard);
+
+    }
 
     if(playerDeck.length < 4)
     {
         console.log("AI wins! You ran out of cards for war.");
+        return;
     }
     else if(aiDeck.length < 4)
     {
         console.log("You win! AI ran out of cards for war.");
+        return;
     }
     else 
     {
@@ -179,18 +199,23 @@ function war()
             console.log("AI play: ", AIWarCardCache[(AIWarCardCache.length-1)].suit, AIWarCardCache[(AIWarCardCache.length-1)].num);
             console.log("You won war!");
             console.log("Cards won:");
+            renderCardsToBrowser(playerWarCardCache[playerWarCardCache.length-1],AIWarCardCache[AIWarCardCache.length-1]);
 
             for(let j=0; j < AIWarCardCache.length; j++)
             {
                 console.log(AIWarCardCache[j].suit, AIWarCardCache[j].num);
                 playerDeck.push(AIWarCardCache[j]);
+                
             }
             for(let x=0; x < playerWarCardCache.length; x++)
             {
                 playerDeck.push(playerWarCardCache[x]);
+                
             }
+            console.log("Your Deck: ", playerDeck.length, "AI Deck: ", aiDeck.length);
             
-
+            recursiveWar = false;
+            return;
         }
         else if(playerWarCardCache[(playerWarCardCache.length-1)].num < AIWarCardCache[(AIWarCardCache.length-1)].num)
         {
@@ -207,9 +232,13 @@ function war()
             {
                 aiDeck.push(AIWarCardCache[y]);
             }
+            console.log("Your Deck: ", playerDeck.length, "AI Deck: ", aiDeck.length);
+            recursiveWar = false;
+            return;
         }
         else {
             // war(playerCard, aiCard);
+            recursiveWar = true;
             war();
         }
 
@@ -232,4 +261,154 @@ function clearWarCardCaches()
 {
     playerWarCardCache = [];
     AIWarCardCache = [];
+}
+
+window.onload = function()
+{
+    this.generateDeck();
+}
+
+function reset()
+{
+    playerDeck = [];
+    aiDeck = [];
+    clearWarCardCaches();
+    generateDeck();
+}
+
+function renderCardsToBrowser(playerCard, aiCard)
+{
+
+    let playerCardConvertedDisplay = '';
+    let playerCardConvertedSuit = '';
+    if(playerCard.num <= 10)
+    {
+        playerCardConvertedDisplay = playerCard.num;
+    }
+    else if(playerCard.num === 11)
+    {
+        playerCardConvertedDisplay = 'J';
+    }
+    else if(playerCard.num === 12)
+    {
+        playerCardConvertedDisplay = 'Q';
+    }
+    else if(playerCard.num === 13)
+    {
+        playerCardConvertedDisplay = 'K';
+    }
+    else if(playerCard.num === 14)
+    {
+        playerCardConvertedDisplay = 'A';
+    }
+
+    if(playerCard.suit === 'spade')
+    {
+        playerCardConvertedSuit = '&spades;'
+    }
+    else if(playerCard.suit === 'heart')
+    {
+        playerCardConvertedSuit = '&hearts;'
+    }
+    else if(playerCard.suit === 'diamond')
+    {
+        playerCardConvertedSuit = '&diamondsuit;'
+    }
+    else if(playerCard.suit === 'club')
+    {
+        playerCardConvertedSuit = '&clubs;'
+    }
+
+    playerDisplayNumOne = document.getElementsByClassName("card-grid-1-1")[0];
+    playerDisplayNumOne.style.color = playerCard.suitColor;
+    playerDisplayNumOne.innerHTML = playerCardConvertedDisplay;
+
+    playerDisplayNumTwo = document.getElementsByClassName("card-grid-1-3")[0];
+    playerDisplayNumTwo.style.color = playerCard.suitColor;
+    playerDisplayNumTwo.innerHTML = playerCardConvertedDisplay;
+
+    playerDisplayNumThree = document.getElementsByClassName("card-grid-3-1")[0];
+    playerDisplayNumThree.style.color = playerCard.suitColor;
+    playerDisplayNumThree.innerHTML = `<br> ${playerCardConvertedDisplay}`;
+
+    playerDisplayNumFour = document.getElementsByClassName("card-grid-3-3")[0];
+    playerDisplayNumFour.style.color = playerCard.suitColor;
+    playerDisplayNumFour.innerHTML = `<br> ${playerCardConvertedDisplay}`;
+
+    playerDisplaySuit = document.getElementsByClassName("card-grid-2-2")[0];
+    playerDisplaySuit.style.color = playerCard.suitColor;
+    playerDisplaySuit.innerHTML = playerCardConvertedSuit;
+    
+    //////////////////////////
+
+    let aiCardConvertedDisplay = '';
+    let aiCardConvertedSuit = '';
+    if(aiCard.num <= 10)
+    {
+        aiCardConvertedDisplay = aiCard.num;
+    }
+    else if(aiCard.num === 11)
+    {
+        aiCardConvertedDisplay = 'J';
+    }
+    else if(aiCard.num === 12)
+    {
+        aiCardConvertedDisplay = 'Q';
+    }
+    else if(aiCard.num === 13)
+    {
+        aiCardConvertedDisplay = 'K';
+    }
+    else if(aiCard.num === 14)
+    {
+        aiCardConvertedDisplay = 'A';
+    }
+
+    if(aiCard.suit === 'spade')
+    {
+        aiCardConvertedSuit = '&spades;'
+    }
+    else if(aiCard.suit === 'heart')
+    {
+        aiCardConvertedSuit = '&hearts;'
+    }
+    else if(aiCard.suit === 'diamond')
+    {
+        aiCardConvertedSuit = '&diamondsuit;'
+    }
+    else if(aiCard.suit === 'club')
+    {
+        aiCardConvertedSuit = '&clubs;'
+    }
+
+    aiDisplayNumOne = document.getElementsByClassName("card-grid-1-1")[1];
+    aiDisplayNumOne.style.color = aiCard.suitColor;
+    aiDisplayNumOne.innerHTML = aiCardConvertedDisplay;
+
+    aiDisplayNumTwo = document.getElementsByClassName("card-grid-1-3")[1];
+    aiDisplayNumTwo.style.color = aiCard.suitColor;
+    aiDisplayNumTwo.innerHTML = aiCardConvertedDisplay;
+
+    aiDisplayNumThree = document.getElementsByClassName("card-grid-3-1")[1];
+    aiDisplayNumThree.style.color = aiCard.suitColor;
+    aiDisplayNumThree.innerHTML = `<br> ${aiCardConvertedDisplay}`;
+
+    aiDisplayNumFour = document.getElementsByClassName("card-grid-3-3")[1];
+    aiDisplayNumFour.style.color = aiCard.suitColor;
+    aiDisplayNumFour.innerHTML = `<br> ${aiCardConvertedDisplay}`;
+
+    aiDisplaySuit = document.getElementsByClassName("card-grid-2-2")[1];
+    aiDisplaySuit.style.color = aiCard.suitColor;
+    aiDisplaySuit.innerHTML = aiCardConvertedSuit;
+
+    return '';
+}
+
+function renderScoresToBrowser()
+{
+    playerDeckLength = playerDeck.length;
+    aiDeckLength = aiDeck.length;
+    document.getElementsByClassName("playerDeckScore")[0].innerHTML = `You: ${playerDeckLength}`;
+    document.getElementsByClassName("aiDeckScore")[0].innerHTML = `AI: ${aiDeckLength}`;
+    return '';
 }
