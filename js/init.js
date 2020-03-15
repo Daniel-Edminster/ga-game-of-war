35d77632342f583d4740d58975c848b30b104b57
Daniel Edminster <Daniel.Edminster@outlook.com>
2020-03-15 13:01:50 -0400

    Fixed console errors caused by remnants of debugging code

--- js/init.js
+++ js/init.js
@@ -1,19 +1,1 @@
 let war = new Deck();
-
-
-function getWidth() {
-    // return Math.max(
-    //   document.body.scrollWidth,
-    //   document.documentElement.scrollWidth,
-    //   document.body.offsetWidth,
-    //   document.documentElement.offsetWidth,
-    //   document.documentElement.clientWidth
-    // );
-
-    return window.outerWidth;
-  }
-
-  setInterval(() => 
-  {
-            document.getElementById("widthDebug").innerHTML = getWidth();
-  }, 10);
